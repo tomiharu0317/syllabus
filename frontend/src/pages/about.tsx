8b12365ee39ca893aa332f284c282646dc803c72
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-21 19:55:06 +0900

    changed color

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -281,7 +281,7 @@ export default function Example() {
             </a>
             <a
               href='#'
-              className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'
+              className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-700 hover:bg-indigo-700'
             >
               Sign up
             </a>
