c28e9ed0506189b65207d4fd67cd2e8d0c63cfd2
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-24 18:42:45 +0900

    Fix sample course

--- frontend/src/pages/course/sample-course.tsx
+++ frontend/src/pages/course/sample-course.tsx
@@ -1,14 +1,11 @@
 import SearchForm from '@/components/molecules/search-form'
 import Course from '@/components/organisms/course'
-import Pagenation from '@/components/molecules/pagination'
 import { NextPage } from 'next'
 
 const CourseRender: NextPage = () => {
   return (
     <div>
-      <SearchForm />
-      <Course />
-      <Pagenation />
+      <h1>sample course</h1>
     </div>
   )
 }
