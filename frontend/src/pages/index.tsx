632b7052385ef005459f08aede9c759798963f3c
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-24 12:08:50 +0900

    delete Home.module.css

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import SearchForm from '@/components/molecules/search-form'
 
 const Home: NextPage = () => {
