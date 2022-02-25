b96e3f952e5fa3976c6383bb7dd3b7154fdec9e9
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-25 22:08:19 +0900

    Fix back button below -> add link to mylist

--- frontend/src/components/organisms/course.tsx
+++ frontend/src/components/organisms/course.tsx
@@ -101,12 +101,14 @@ export default function Course() {
             </dl>
           </div>
           <div className='py-6 text-center'>
-            <button
-              type='button'
-              className='relative inline-flex items-center px-4 py-2 mr-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200'
-            >
-              Back
-            </button>
+            <Link href={'/mylist'}>
+              <button
+                type='button'
+                className='relative inline-flex items-center px-4 py-2 mr-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200'
+              >
+                Back
+              </button>
+            </Link>
           </div>
         </div>
       </div>
