70ffea3355eb96846059a7d65c8cbb86b258f034
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-25 15:38:14 +0900

    Add course detail page

--- frontend/src/pages/course/sample-course.tsx
+++ frontend/src/pages/course/sample-course.tsx
@@ -1,13 +1,8 @@
-import SearchForm from '@/components/molecules/search-form'
 import Course from '@/components/organisms/course'
 import { NextPage } from 'next'
 
 const CourseRender: NextPage = () => {
-  return (
-    <div>
-      <h1>sample course</h1>
-    </div>
-  )
+  return <Course />
 }
 
 export default CourseRender
