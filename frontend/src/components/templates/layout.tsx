fe751b3b1d7e4a4f1526596d5fba22687b94c675
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-26 15:39:52 +0900

    Fix layout to fix footer at the bottom

--- frontend/src/components/templates/layout.tsx
+++ frontend/src/components/templates/layout.tsx
@@ -7,8 +7,7 @@ export default function Layout({ children }: any) {
     <>
       <div className='flex flex-col min-h-screen'>
         <Header />
-        {/* <main className='flex-1'>{children}</main> */}
-        <main className=''>{children}</main>
+        <main className='flex-1'>{children}</main>
         <Footer />
       </div>
     </>
