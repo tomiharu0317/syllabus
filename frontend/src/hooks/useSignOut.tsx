95edc92f7d2e190124c78e14a72348fdddd21017
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-28 21:17:24 +0900

    Add func to redirect to login page if user hasnt logged in

---
+++ frontend/src/hooks/useSignOut.tsx
@@ -0,0 +1,12 @@
+// import { useSession, signOut } from 'next-auth/react'
+// import { useRouter } from 'next/router'
+
+// export function useSingOut() {
+//   const router = useRouter()
+
+//   signOut()
+
+//   router.push('/')
+
+//   return
+// }
