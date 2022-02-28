8ffb6810f4b18f0d0042da06677f3b2e37b6735a
tomiharu0317 <tomiharu1017@gmail.com>
2022-02-28 23:55:32 +0900

    Fix timetable button link whether current is mylist or mylist/timetable

--- frontend/src/components/atoms/button.tsx
+++ frontend/src/components/atoms/button.tsx
@@ -1,20 +1,31 @@
 import Link from 'next/link'
 import { useLocale } from '@/hooks/lang/lang-locale'
+import { useRouter } from 'next/router'
 
 /* This example requires Tailwind CSS v2.0+ */
 export default function Example() {
   const { t } = useLocale()
+  const router = useRouter()
 
   return (
     <>
       <div className='flex justify-end'>
         <div className='flex-shrink-0 px-6 pt-8 pb-0 sm:pt-10 sm:pb-4'>
           <div className='flex px-4 jusitify-end x-auto sm:px-6 md:px-8'>
-            <Link href={'/mylist/timetable'}>
-              <button type='button' className='white-button btn'>
-                {t.TIME_TABLE}
-              </button>
-            </Link>
+            {/* mylistページなら時間割ボタン, timetableページなら戻るボタン */}
+            {router.pathname === '/mylist' ? (
+              <Link href={'/mylist/timetable'}>
+                <button type='button' className='white-button btn'>
+                  {t.TIME_TABLE}
+                </button>
+              </Link>
+            ) : (
+              <Link href={'/mylist'}>
+                <button type='button' className='white-button btn'>
+                  {t.BACK}
+                </button>
+              </Link>
+            )}
           </div>
         </div>
       </div>
